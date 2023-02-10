299c94da5742eaa7c8b8319d8497bd76839f51e6
flotry <jastbast1103@outlook.com>
2023-02-10 20:36:37 +0300

    switch lession commit

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ if (q === 7) {
 else {
     console.log('Incurrect');
 }
+
+switch (q) {
+    case 7:
+        console.log('Currect');
+        break;
+    default:
+        console.log('Incurrect');
+}
+
+
+const num = Number(prompt('Insert number'));
+
+switch (true) {
+    case num > 0:
+        console.log('> 0');
+        break;
+    case num < 0:
+        console.log('< 0');
+        break;
+    default:
+        console.log('null');
+}

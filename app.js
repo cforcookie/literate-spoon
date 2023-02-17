73d36567dd805bcc1a97a88798fab3f9b4946099
flotry <jastbast1103@outlook.com>
2023-02-17 21:48:58 +0300

    1

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const bmwX3Price = 100000;
-const budget = 20000;
+/*
+Методом prompt получите ответ пользователя на вопрос 
+"Сколько будет 7 + или - 15?". Если ответ верен, выведите в 
+консоли "Успех", если нет - "Вы робот!", а если он 
+введёт "Я не робот", то тоже "Успех".
+*/
 
-let message;
+// const anser = prompt("Сколько будет 7 + или - 15?");
 
-console.log(`I want buy ${budget > bmwX3Price ? 'BMW' : 'Cycle'}`);
+// switch (true) {
+//     case Number(anser) === 22:
+//     case Number(anser) === -8:
+//     case 'Я не робот':
+//         console.log('Успех');
+//         break;
+//     default:
+//         console.log('Вы робот');    
+// }
+console.log(String('10') - 5 + Boolean('') + '1');
+
+console.log(true && false || false);
+
+console.log(!'' && 'Yes' || !('No' && 'false'))

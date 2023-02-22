d505fdc0f3565868f7de6962dd8199968af72150
flotry <jastbast1103@outlook.com>
2023-02-22 20:02:00 +0300

    arrays

--- app.js
+++ app.js
@@ -1,30 +1,13 @@
-const guyAge = 25;
-const heHaveWork = true;
-const haveMoney = 1500;
+const users = ['Anna', 'Vika', 'Kate'];
+console.log(users);
+users[2] = 'Kriss';
+console.log(users);
 
-let cheakMoney = (money, credit) => {
-    if (money + credit >= 2000) {
-        return true;
-    }
-    return false;
-}
+users.push('Nikita');
+users.unshift('Vasa');
+console.log(users);
+//Удвляют последнй и первый элементы в массиве и возвращают их
+users.pop();
+users.shift();
+console.log(users);
 
-function prufCredit(age, haveWork, money) {
-    let credit;
-    
-    if (haveWork == true && age > 24) {
-        credit = 500;
-        return cheakMoney(money, credit);
-
-    } if (haveWork == false && age > 24) {
-        credit = 100;
-        return cheakMoney(money, credit);
-    }
-    credit = 0;
-    return cheakMoney(money, credit);
-    
-}
-
-const result = prufCredit(guyAge, heHaveWork, haveMoney);
-
-console.log(result);

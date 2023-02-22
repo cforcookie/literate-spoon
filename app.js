dc7b039d25221f69cd34b4b103d65c7cd6ea4136
flotry <jastbast1103@outlook.com>
2023-02-22 20:38:54 +0300

    arrays prikol

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
-const roles = ['admin', 'user', 'manager'];
+const tasks = ['exes 1'];
 
-const url = 'auth/user/login';
-const res = url.split('/');
-console.log(res);
-console.log(roles.join(' '))
+function Add (name) {
+    tasks.push(name);
+}
+
+function Remove (name) {
+    const index = tasks.indexOf(name);
+
+    if (index === -1) {
+        return
+    }
+
+    tasks.splice(index, 1);
+}
+
+function Prioritize (name) {
+    const index = tasks.indexOf(name);
+    const oldTask = tasks[index];
+    
+    if (index === -1) {
+        return
+    }
+    
+    tasks.splice(index, 1);
+    tasks.unshift(oldTask);
+}
+
+Add('new 1');
+Add('new 2');
+console.log(tasks);
+Remove('new 2');
+console.log(tasks);

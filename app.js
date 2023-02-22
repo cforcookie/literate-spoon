23e246810a27a329a1c94f90099e631107fea08a
flotry <jastbast1103@outlook.com>
2023-02-22 20:56:46 +0300

    rest

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-const tasks = ['exes 1'];
-
-function Add (name) {
-    tasks.push(name);
-}
-
-function Remove (name) {
-    const index = tasks.indexOf(name);
-
-    if (index === -1) {
-        return;
-    }
-
-     return tasks.splice(index, 1);
-}
-
-function Prioritize (name) {
-    const result = Remove(name);
-    if (!result) {
-        return;
-    }
-    tasks.unshift(result[0]);
-}
-
-Add('new 1');
-Add('new 2');
-console.log(tasks);
-Remove('new 2');
-console.log(tasks);
-Prioritize('new 1');
-console.log(tasks);
+const  data = [1, 2, 3, 4, 5, 6];
+const [one, two, ...others] = data;
+console.log(one, two, others);

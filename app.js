650797ab791cd67d4c2d4faa81541750d096f597
flotry <jastbast1103@outlook.com>
2023-02-24 23:46:15 +0300

    breake continue

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-for (let i = 0; i < 10; i++) {
-    console.log(res = i + 1);
+const tasks = ['Task 1', 'Task 2', 'Task 3'];
+
+for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Task 2') {
+        continue;
+    }
+    console.log(tasks[i]);
+}
+console.log('-----------------');
+for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Task 2') {
+        break;
+    }
+    console.log(tasks[i]);
 }

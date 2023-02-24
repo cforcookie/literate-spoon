e18fcc242639c928d8d57bd55f62961044e6e409
flotry <jastbast1103@outlook.com>
2023-02-25 00:23:48 +0300

    Тренеровочное задание

--- app.js
+++ app.js
@@ -1,8 +1,41 @@
-const arr = [1, 2, 3, 4];
-for (let element of arr) {
-    console.log(element);
+const operations = [1000, -700, 300, -500, 10000];
+let balance = 100;
+function FinalBalance () {
+    for (let element of operations) {
+        balance+=element;
+    }
+    console.log(balance);
 }
 
-for (let element in arr) {
-    console.log(element);
+function ZerolessBalance () {
+    for (let element of operations) {
+        balance+=element;
+        if (balance < 0) {
+            return false;
+        }
+    }
+    console.log(true);
 }
+function Mid () {
+    let i = 1;
+    let j = 1;
+    let less = 0;
+    let more = 0;
+    for (let element of operations) {
+        if (element > 0) {
+            more+=element;
+            i++
+        }
+        if (element < 0) {
+            less+=element;
+            j++
+        }
+    }
+    i = more / i;
+    j = less / j;
+    console.log(i, j);
+}
+
+FinalBalance();
+ZerolessBalance();
+Mid();

cf4995742f8e2bad44d305bff1ed19b84b02c79a
flotry <jastbast1103@outlook.com>
2023-03-07 02:25:42 +0300

    some method

--- app.js
+++ app.js
@@ -1,41 +1,8 @@
-const operations = [1000, -700, 300, -500, 10000];
-let balance = 100;
-function FinalBalance () {
-    for (let element of operations) {
-        balance+=element;
-    }
-    console.log(balance);
+const arr = [2, 4, 4, 10, 20];
+
+function some (element, arr) {
+    console.log(arr.find(el => el == element));
 }
 
-function ZerolessBalance () {
-    for (let element of operations) {
-        balance+=element;
-        if (balance < 0) {
-            return false;
-        }
-    }
-    console.log(true);
-}
-function Mid () {
-    let i = 1;
-    let j = 1;
-    let less = 0;
-    let more = 0;
-    for (let element of operations) {
-        if (element > 0) {
-            more+=element;
-            i++
-        }
-        if (element < 0) {
-            less+=element;
-            j++
-        }
-    }
-    i = more / i;
-    j = less / j;
-    console.log(i, j);
-}
-
-FinalBalance();
-ZerolessBalance();
-Mid();
+some(10, arr);
+console.log(arr.some(el => el === 0))
